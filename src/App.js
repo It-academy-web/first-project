cb7bf5515af4112d7306b60cb96590f05aa9ccc2
unknown <you@example.com>
2020-06-18 11:40:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p>asdas</p>
+        <p>asdsd</p>
+        <p>asdasdasd</p>
+        <p>asdasdasd</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
